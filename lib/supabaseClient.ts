333543bfd1e8b15833c9ff5b365566b86a622bef
micabona <108426694+micabona@users.noreply.github.com>
2025-09-03 15:16:41 -0700

    Update supabaseClient.ts

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,5 +1,1 @@
-const supabasePackage = require('@' + 'supabase/supabas-js');
-const supabaseUrl: string = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseAnonKey: string = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
-
-export const supabase = supabasePackage['create' + 'Client'](supabaseUrl, supabaseAnonKey);
+export const supabase = {};
